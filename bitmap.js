64242d9ee52dd6488d57e8960905e3dc161dc0ae
teak1cd <isenne@sterneschool.org>
2016-11-02 14:24:35 -0700

    Update bitmap.js

--- bitmap.js
+++ bitmap.js
@@ -1,1 +1,12 @@
-
+function makeGrid(width,height){
+  var width = width || document.getElementById("image_width").value+1;
+  var height = height || document.getElementById("image_height").value+1;
+  var grid = "";
+  for(var y = 0;y<height;y++){
+    for(var x = 0;x<width;x++){
+      grid+="<button onClick=\"console.log('hi');\" id=\""+x+"_"+y+"\">&nbsp;</button>";
+    }
+    grid+="<br/>";
+  }
+  return grid;
+}
